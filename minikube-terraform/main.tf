cc148aeb770b8367b416aec5d07c58e29ede0ded
Akshay Kapoor <akshay.kapoor@westwing.de>
2019-08-28 00:17:33 +0200

    Modified pipeline and changed ingress rule

--- minikube-terraform/main.tf
+++ minikube-terraform/main.tf
@@ -26,13 +26,22 @@ resource "aws_security_group" "esk8s_instance_sg" {
   name_prefix   = "esk8s_instance_sg"
 }
 
-resource "aws_security_group_rule" "sg_ingress" {
+resource "aws_security_group_rule" "esk8s_sg_ingress_1" {
   from_port         = 0
   protocol          = "All"
   security_group_id = "${aws_security_group.esk8s_instance_sg.id}"
   to_port           = 65535
   type              = "ingress"
   cidr_blocks       = ["92.218.249.130/32"]
+}
+
+resource "aws_security_group_rule" "esk8s_sg_ingress_2" {
+  from_port         = 80
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.esk8s_instance_sg.id}"
+  to_port           = 80
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "sg_egress" {
